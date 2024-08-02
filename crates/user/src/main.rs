822fd7e9e8b28f4270a4a9163acd6f67c66966a6
Sotatek-DucPham <duc.pham@sotatek.com>
2024-08-02 13:36:57 +0700

    feat: more

--- crates/user/src/main.rs
+++ crates/user/src/main.rs
@@ -8,6 +8,8 @@ async fn root() -> &'static str {
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
     let app = Router::new().route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
