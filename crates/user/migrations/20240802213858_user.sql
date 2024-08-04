8d3374a3a921bf0a41ffbb9f675e1e5db9d82ea4
Sotatek-Ducpham <duc.pham@sotatek.com>
2024-08-04 18:24:17 +0700

    feat: more

--- crates/user/migrations/20240802213858_user.sql
+++ crates/user/migrations/20240802213858_user.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS "user";
+
 CREATE TABLE "user" (
-    id VARCHAR(255)
+    id VARCHAR(255),
+    email VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255)
 );
--- crates/user/src/main.rs
+++ crates/user/src/main.rs
@@ -1,45 +1,13 @@
-use std::collections::HashMap;
-
-use axum::{
-    async_trait,
-    extract::{FromRequestParts, Path},
-    http::{request::Parts, StatusCode},
-    response::{IntoResponse, Response},
-    routing::get,
-    RequestPartsExt, Router,
-};
+use axum::{routing::get, Json, Router};
 use clap::Parser;
-use user::config::Config;
+use serde::Serialize;
+use user::{config::Config, version::Version};
 
-#[derive(Debug)]
-enum Version {
-    V1,
-}
+#[derive(Serialize)]
+pub struct EmptyResponse {}
 
-#[async_trait]
-impl<S> FromRequestParts<S> for Version
-where
-    S: Send + Sync,
-{
-    type Rejection = Response;
-
-    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        let params: Path<HashMap<String, String>> =
-            parts.extract().await.map_err(IntoResponse::into_response)?;
-
-        let version = params
-            .get("version")
-            .ok_or_else(|| (StatusCode::NOT_FOUND, "version param missing").into_response())?;
-
-        match version.as_str() {
-            "v1" => Ok(Version::V1),
-            _ => Err((StatusCode::NOT_FOUND, "unknown version").into_response()),
-        }
-    }
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root() -> Json<EmptyResponse> {
+    Json(EmptyResponse {})
 }
 
 async fn handler(version: Version) {
@@ -51,8 +19,7 @@ async fn main() {
     dotenv::dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let config = Config::parse();
-    println!("db {}", config.database_url);
+    let _config = Config::parse();
 
     let app = Router::new()
         .route("/", get(root))
