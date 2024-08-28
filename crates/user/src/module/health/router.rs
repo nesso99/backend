ebc40546ce7fa3af39e4846d7cff92fec03b484a
nesso99 <ducpham6677@gmail.com>
2024-08-28 11:26:15 +0700

    feat: yata try

--- crates/user/src/module/health/router.rs
+++ crates/user/src/module/health/router.rs
@@ -1,5 +1,8 @@
 use axum::{response::IntoResponse, routing::get, Json, Router};
 use serde_json::json;
+use yata::helpers::RandomCandles;
+use yata::indicators::MACD;
+use yata::prelude::*;
 
 use crate::state::AppState;
 
@@ -7,10 +10,22 @@ pub struct HealthRouter;
 
 impl HealthRouter {
     pub fn new_router() -> Router<AppState> {
-        Router::new().route("/", get(root))
+        Router::new()
+            .route("/", get(root))
+            .route("/yata", get(yata))
     }
 }
 
 async fn root() -> impl IntoResponse {
     Json(json!({}))
+}
+
+async fn yata() -> impl IntoResponse {
+    let candles: Vec<_> = RandomCandles::new().take(100).collect();
+    let mut iter = candles.iter().cycle();
+    let mut indicator = MACD::default().init(iter.next().unwrap()).unwrap();
+    for _ in 0..50 {
+        indicator.next(iter.next().unwrap());
+    }
+    format!("{:?}", indicator).into_response()
 }
